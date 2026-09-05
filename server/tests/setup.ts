a6f834e922315b7bda1b865c16534e58ddd43ed0
Pratham <pratham3407@gmail.com>
2026-09-06 01:26:50 +0530

    fixing the backend

--- server/tests/setup.ts
+++
@@ -1,7 +0,0 @@
-import { afterAll } from 'vitest';
-import { disconnectPrisma } from '../src/db/prisma';
-
-/** Runs in every worker. Migrations are applied once by globalSetup.ts. */
-afterAll(async () => {
-  await disconnectPrisma();
-});
